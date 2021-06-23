dd0039a2db696f82d8e208ec71b3d2703505729f
Abdil Tegar Arifin <abdiltegar12@gmail.com>
2021-06-23 10:56:06 +0700

    Membuat Fungsi Read Data

---
+++ app/src/main/java/com/example/testbarang/adapter/AdapterLihatBarang.java
@@ -0,0 +1,68 @@
+package com.example.testbarang.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.testbarang.Barang;
+import com.example.testbarang.R;
+
+import java.util.ArrayList;
+
+public class AdapterLihatBarang  extends RecyclerView.Adapter<AdapterLihatBarang.ViewHolder> {
+
+    private ArrayList<Barang> daftarBarang;
+    private Context context;
+
+    public AdapterLihatBarang(ArrayList<Barang> barangs, Context ctx){
+        daftarBarang = barangs;
+        context = ctx;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvTitle;
+
+        ViewHolder(View v){
+            super(v);
+            tvTitle = (TextView) v.findViewById(R.id.tv_namabarang);
+        }
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_barang, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        final String name = daftarBarang.get(position).getNama();
+
+        holder.tvTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        holder.tvTitle.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                return true;
+            }
+        });
+
+        holder.tvTitle.setText(name);
+    }
+
+    @Override
+    public int getItemCount() {
+        return daftarBarang.size();
+    }
+}
